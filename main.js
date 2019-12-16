e1d4687cbecf60ed4cd654e964a3e7bf7c5cf39c
Prakhar <prkhrsrvstv1@gmail.com>
2019-12-17 00:33:42 +0530

    Implement content hashing

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
+function hashCode(input) {
+	var hash = 0, i, chr;
+  if (input.length === 0) return hash;
+  for (i = 0; i < input.length; i++)
+    hash = ((hash << 5) - hash) + input.charCodeAt(i);
+  return hash;
+}
+
 function dumpText() {
 	console.log("dumpText was called");
-	var contents = document.getElementById("text-contents").value;
-	alert(contents);
+	const contents = document.getElementById("text-contents").value;
 	// Writing the contents to a file.
 	// 1. Record datetime
 	// 2. Create hash with file contents + datetime(UTC)
 	// 3. Create a file with the contents
-	// 4. Put into the record table: hash, title, created_on
+	// 4. Put into the record table: hash, created_on
+	const datetime = Date.now();
+	var hashValue = hashCode(contents.slice(0, 5) + datetime + contents.slice(-5))
+	console.log(hashValue);
+	var blob = new Blob([contents], { type: "text/plain;charset=utf-8" });
+	saveAs(blob, hashValue);
 }
 
 function dumpFile() {
